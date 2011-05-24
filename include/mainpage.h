1d1fbe03575acb3a3065ec1af818936e1fe2f93a
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-05-24 19:07:48 +0000

    2011-05-24	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* README.doxygen, include/Doxyfile, include/mainpage.h: New files.

---
+++ include/mainpage.h
@@ -0,0 +1,25 @@
+/*
+ *  COPYRIGHT (c) 1989-2011.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+/**
+ * @mainpage
+ *
+ * The ... TBD description of package
+
+ *
+ *  @section APIDeviceClasses API Device Classes
+ *
+ * This includes APIs tailored to each of the following device classes.
+ *
+ * - Analog to Digital Converters (ADCs)
+ * - Digital to Analog Converters (DACs)
+ * - XXX ...
+ */
